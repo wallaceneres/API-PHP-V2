af05197a5b958f9170bab4338730e3de071d6ec6
Wallace Neres Pereira de Souza <wallace.souza@groupsoftware.com.br>
2025-07-15 11:27:50 -0300

    Creating admin area

--- app/inc/config.php
+++ app/inc/config.php
@@ -3,3 +3,5 @@
 define('API_BASE_ENDPOINT' , 'http://127.0.0.1/API-PHP-V2/api/');
 define('API_USER', 'KkCaG1Jutho4usqBL8x2MXjaeY1X2xdc');
 define('API_PASS', 'Va6hqwxNaZS0WLCYqDzJnf1kFAi5zaia');
+
+//password encoded $2y$10$F8riTFmr.OKps6DZUUhPqOXULNX/VslV0kCW.GldIhwD4GO.HU5NC
