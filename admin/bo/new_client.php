a57c47d7972bc79b1f251c29fc1e9043221a9c2f
Wallace Neres <wallace.souzza@live.com>
2025-09-17 20:33:40 -0300

    Added the function to generate user and password tokens

--- admin/bo/new_client.php
+++ admin/bo/new_client.php
@@ -14,12 +14,17 @@
             </div>
             <div class="mb-3">
                 <label class="form-label">Username:</label>
-                <input type="text" name="text_usuario" class="form-control" required>
+                <input type="text" name="text_usuario" id="usuario" class="form-control" readonly required>
             </div>
             <div class="mb-3">
                 <label class="form-label">Senha:</label>
-                <input type="text" name="text_senha" class="form-control" required>
+                <input type="text" name="text_senha" id="senha" class="form-control" readonly required>
             </div>
+
+            <div class="mb3 text-end" onclick="gerarUsuarioPassword()">
+                <button class="btn btn-primary">Gerar</button>
+            </div>
+
             <div class="mb-3 text-center">
                 <a href="?r=home" class="btn btn-secondary btn-150">Cancelar</a>
                 <input type="submit" value="Criar" class="btn btn-primary btn-150">
@@ -27,3 +32,33 @@
         </div>
     </div>
 </div>
+<script>
+    function gerarUsuarioPassword()
+    {
+        // definir variaveis
+        let client_username = "";
+        let client_password = "";
+        let charbase ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        let client_username_length = 32;
+        let client_password_length = 32;
+
+        //client username
+        for(var i = 1; i <= client_username_length; i++)
+        {
+            client_username += charbase.charAt(Math.floor(Math.random()*charbase.length));
+        }
+
+        //client password
+        for(var i = 1; i <= client_password_length; i++)
+        {
+            client_password += charbase.charAt(Math.floor(Math.random()*charbase.length));
+        }
+
+        // colocar os valores nos inputs
+
+        document.querySelector("#usuario").value = client_username;
+
+        document.querySelector("#senha").value = client_password;
+    }
+</script>
