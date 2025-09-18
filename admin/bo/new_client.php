afbcb6e9578d8410b48940f61f5a19deaed27bed
Wallace Neres <wallace.souzza@live.com>
2025-09-17 21:06:19 -0300

    API user logic initiated

--- admin/bo/new_client.php
+++ admin/bo/new_client.php
@@ -1,34 +1,93 @@
 <?php
     defined('ROOT') or die('Acesso inválido');
     require_once('bo/navegacao.php');
+
+    //no caso de existir um post
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        /*
+        valiidacao de dados
+        - verificar se todos os dados vieramo no post
+        - verificar se existe cliente com mesmo nome no banco
+        - verificar se existe cliente com o mesmo username
+        - inserir o usuario na bd
+        - voltar para a pagina inicial
+        */
+
+        $error = '';
+
+        if(!isset($_POST['text_cliente']) || 
+        !isset($_POST['text_usuario']) || 
+        !isset($_POST['text_senha'])
+        )
+        {
+            $error = "Não foram fornecidos todos os dados";
+        }
+
+        if(empty($error))
+        {
+            $cliente = $_POST['text_cliente'];
+            $usuario = $_POST['text_usuario'];
+            $senha = $_POST['text_senha'];
+
+            $bd = new database();
+
+            $parametros = [
+                ':cliente' => $cliente
+            ];
+
+            $resultados = $bd->EXE_QUERY("SELECT * FROM authentication WHERE client_name = :cliente", $parametros);
+
+            if(!empty($resultados)){
+                $error = "Já existe um cliente com o mesmo nome";
+            }  
+        }
+
+        if(empty($error))
+        {
+            die('OK');
+        }
+
+    }
     ?>
 
 <div class="container">
     <div class="row mt-5">
         <div class="col-sm-6 offset-sm-3">
-            <h3 class="text-center">Novo Cliente</h3>
-            <hr>
-            <div class="mb-3">
-                <label class="form-label">Cliente:</label>
-                <input type="text" name="text_cliente" class="form-control" required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Username:</label>
-                <input type="text" name="text_usuario" id="usuario" class="form-control" readonly required>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Senha:</label>
-                <input type="text" name="text_senha" id="senha" class="form-control" readonly required>
-            </div>
 
-            <div class="mb3 text-end" onclick="gerarUsuarioPassword()">
-                <button class="btn btn-primary">Gerar</button>
-            </div>
+            <form action="?r=new_client" method="post">
+                <h3 class="text-center">Novo Cliente</h3>
+                <hr>
+                <div class="mb-3">
+                    <label class="form-label">Cliente:</label>
+                    <input type="text" name="text_cliente" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Username:</label>
+                    <input type="text" name="text_usuario" id="usuario" class="form-control" readonly required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Senha:</label>
+                    <input type="text" name="text_senha" id="senha" class="form-control" readonly required>
+                </div>
 
-            <div class="mb-3 text-center">
-                <a href="?r=home" class="btn btn-secondary btn-150">Cancelar</a>
-                <input type="submit" value="Criar" class="btn btn-primary btn-150">
-            </div>
+                <div class="mb3 text-end" onclick="gerarUsuarioPassword()">
+                    <button class="btn btn-primary">Atualizar</button>
+                </div>
+
+                <div class="mb-3 text-center">
+                    <a href="?r=home" class="btn btn-secondary btn-150">Cancelar</a>
+                    <input type="submit" value="Criar" class="btn btn-primary btn-150">
+                </div>
+            </form>
+
+            <?php if(!empty($error)): ?>
+                <p class="alert alert-danger p-2 text-center">
+                    <?= $error ?>
+                </p>
+            <?php endif; ?>
+
         </div>
     </div>
 </div>
@@ -61,4 +120,8 @@
 
         document.querySelector("#senha").value = client_password;
     }
+
+    //gerar usuario e senha
+    gerarUsuarioPassword();
 </script>
+
